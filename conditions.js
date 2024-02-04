6dc4aaaf8994ff3c0d940b28646eb5b334dd0bbe
gulsah <gmutluayyigit@gmail.com>
2024-02-04 15:08:26 +0300

    son

--- conditions.js
+++ conditions.js
@@ -1,24 +1,19 @@
-
-
-console.log("*** CONDITIONS ***")
-
+console.log("*** CONDITIONS ***");
 
 // const num1 = Number(prompt("Bir sayi giriniz:"))
- const num1 = 5
+const num1 = 5;
 // console.log(num1, typeof num1);
 // console.log("Toplam:", num1 + num2);
 
 if (num1 < 0) {
-    console.log("Girilen sayı negatiftir");
-}
-else if (num1 > 0) {
-    console.log("Girilen sayı pozitiftir")
-}
-else {
-    console.log("Girilen sayi sıfırdır")
+  console.log("Girilen sayı negatiftir");
+} else if (num1 > 0) {
+  console.log("Girilen sayı pozitiftir");
+} else {
+  console.log("Girilen sayi sıfırdır");
 }
 
-//   
+//
 // const enBuyuk = Math.max(sayi1, sayi2, sayi3)
 // console.log("EnBuyuk:", enBuyuk)
 
@@ -48,45 +43,95 @@ else {
 //     console.log("Enbuyuk:", sayi3);
 // }
 
- const sayi1 = 5
- const sayi2 = 6
- const sayi3 = 7
+//  const sayi1 = 5
+//  const sayi2 = 6
+//  const sayi3 = 7
 
- let enBuyuk = sayi1
+//  let enBuyuk = sayi1
 
- if (sayi2 > enBuyuk ) {
-    enBuyuk = sayi2
- }
+//  if (sayi2 > enBuyuk ) {
+//     enBuyuk = sayi2
+//  }
 
- if (sayi3 > enBuyuk) {
-    enBuyuk = sayi3
- }
+//  if (sayi3 > enBuyuk) {
+//     enBuyuk = sayi3
+//  }
 
- console.log("EnBuyuk:", enBuyuk)
+//  console.log("EnBuyuk:", enBuyuk)
 
+const yaş = 20;
+const cinsiyet = "Erkek";
+const sağlıklı = true;
+const koşul =
+  yaş >= 18 && cinsiyet.toLowerCase() === "erkek" && sağlıklı === true;
 
+if (koşul === true) {
+  console.log("askerlik yapmalı");
+} else {
+  console.log("askerlik yapması gerekmez");
+}
 
+const cumle = "Bugün günlerden Cumartesi";
+const kucukHarfler = cumle.toLowerCase();
+const buyukHarfler = cumle.toUpperCase();
+console.log(kucukHarfler);
+console.log(buyukHarfler);
 
+const yas = 15;
+//? Ternary
+yas >= 18 ? console.log("askerlik yapmalı") : console.log("askerlik yapmaz");
 
+//? if-else
+if (yas > 18) {
+  console.log("askerlik yapmalı");
+} else {
+  console.log("askerlik yapmaz");
+}
 
+const age = 30;
 
+const sonuc = age <= 14 ? "cocuk" : age <= 18 ? "ergen" : "yetişkin";
 
+console.log(sonuc);
 
+//? if-else hali
 
+let durum = "";
+if (age <= 14) {
+  durum = "cocuk";
+} else if (age <= 18) {
+  durum = "ergen";
+} else {
+  durum = "yetiskin";
+}
+console.log(durum);
 
+//! ornek
+const calismaSuresi = 10;
+let maas = 30000;
 
+// calismaSuresi >= 10 ? (maas = maas * 1.5) : "";
 
+maas = calismaSuresi >= 10 ? maas * 1.5 : maas
+console.log("MAAS:" , maas);
 
+let salary = 40000
+let year = 5
+let marriage = true
 
+// year >= 10 && marriage === true && (salary = salary * 1.5) // 2 condition ve tek durum
 
+year >= 10 && (salary = salary * 1.5) // tek condition tek durum
 
+console.log("Salary:", salary);
 
+const sene = 14
+let result = "ahmet"
 
+sene >= 18 || (result = "cocuk")
+console.log(result);
 
-
-
-
-
-
-
-
+//false ise çalışır
+result = ""
+let myVar = result || "noname"
+console.log(myVar);
